0bcfb83881f8d166d7e5653c28f222c4bb426768
Gloria <izayura@gmail.com>
2019-06-27 18:33:53 -0500

    experimentos de css

--- src/data.js
+++ src/data.js
@@ -3,14 +3,21 @@ window.dataLovers = {
     filterData:(RICKANDMORTY, condition)=>
   {
      //aplicar el filtro a la información
-      let impresion = RICKANDMORTY.filter((personaje) => 
+      let impresion = RICKANDMORTY.filter((personaje) =>
       {
         //sustitución
-        return personaje.status === condition
-      })//transformar el objeto en string en pantalla
+        return personaje.status === condition;
+      });//transformar el objeto en string en pantalla
         let myJSON = JSON.stringify(impresion);
 
       return myJSON;
+
+    /* EXPERIMENTOS PARA QUE ARROJE LOS DATOS SEPARADOS
+      let filtering= RICKANDMORTY.filter((idStatus)=>{
+        return idStatus.status === condition
+      })
+        let objectText= JSON.stringify(filtering);
+      return objectText*/
 },
 
   sortData:(/*data, sortBy, sortOrder*/)=>{
